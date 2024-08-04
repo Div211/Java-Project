cd83e165f4dfd6348a3791dfb67658bf33694917
Divyanshu Mittal <94476659+Div211@users.noreply.github.com>
2024-08-04 12:18:51 +0530

    Update view.java

--- Project Qspiders/store/src/edu/jasp/store/view/view.java
+++ Project Qspiders/store/src/edu/jasp/store/view/view.java
@@ -1,8 +1,9 @@
 package edu.jasp.store.view;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 import edu.jasp.store.controller.controller;
+import edu.jasp.store.model.Product;
 import edu.jasp.store.model.Store;
 
 public class view {
@@ -28,7 +29,7 @@ public class view {
 	public static void main(String[] args) {
 	do {
 		System.out.println("Select option from below list: ");
-		System.out.println("1.Display store information\n2.Add product\n3.Display all product\n4.Remove product\n0.Exit");
+		System.out.println("1.Display store information\n2.Add product\n3.Display all product\n4.Remove product\n5.Remove Product\n6.Add products\n0.Exit");
 		System.out.println("Enter digit respective to desired option: ");
 		byte userChoice = myInput.nextByte();
 		myInput.nextLine();
@@ -41,6 +42,7 @@ public class view {
 			System.out.println("Store: "+store+"\n");
 			break;
 		case 2:
+			controller.addProduct(getProductDetails());
 			System.out.println(store.getProducts());
 			break;
 		case 3:
@@ -52,6 +54,13 @@ public class view {
 		case 5:
 			
 			break;
+		case 6:
+			if(store.getProducts() != null) 
+				controller.addProducts(addProducts());
+			else
+			controller.addProducts(addProducts());
+			System.out.println(store.getProducts());
+			break;
 		default:
 			System.out.println("Invalid choice");
 			break;
@@ -60,6 +69,34 @@ public class view {
 	}while(true);
 	
 	}
+	public static Product getProductDetails() {
+		System.out.println("Enter id: ");
+		int productId = myInput.nextInt();
+		myInput.nextLine();
+		System.out.println("Enter name: ");
+		String productName = myInput.nextLine();
+		System.out.println("Enter price: ");
+		double productPrice = myInput.nextInt();
+		myInput.nextLine();
+		System.out.println("Enter Quantity: ");
+		int productQuantity = myInput.nextInt();
+		myInput.nextLine();
+		boolean productAvailibility = true;
+		if(productQuantity<=0) {
+			productAvailibility = false;
+		}
+		return new Product(productId, productName, productPrice, productQuantity, productAvailibility);
+	}
 	
-	 
+	public static List<Product> addProducts() {
+		ArrayList<Product> newProductList = new ArrayList<Product>();
+		boolean toContinue = true;
+		do {
+			newProductList.add(getProductDetails());
+			System.out.println("Continue adding products ? y/n: ");
+			if(myInput.next().charAt(0)=='n') 
+				toContinue = false;
+			}while(toContinue);
+			return newProductList;
+		}
 }
