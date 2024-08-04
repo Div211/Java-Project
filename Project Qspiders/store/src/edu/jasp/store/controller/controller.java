94b409ff3d31abc037e7b238edb333751db28c96
Divyanshu Mittal <94476659+Div211@users.noreply.github.com>
2024-08-04 12:19:20 +0530

    Update controller.java

--- Project Qspiders/store/src/edu/jasp/store/controller/controller.java
+++ Project Qspiders/store/src/edu/jasp/store/controller/controller.java
@@ -1,5 +1,7 @@
 package edu.jasp.store.controller;
-
+import java.util.ArrayList;
+import java.util.List;
+import edu.jasp.store.model.Product;
 import edu.jasp.store.model.Store;
 
 public class controller {
@@ -8,7 +10,13 @@ public class controller {
 	public Store getStore() {
 		
 		return store = new Store();
-		
-		
+	}
+	public void addProduct(Product product) {
+		store.setProducts(new ArrayList<Product>());	
+		List<Product> products = store.getProducts();
+		products.add(product);
+		}
+	public void addProducts(List<Product>newProduct){
+		store.getProducts().addAll(newProduct);
 	}
 }
